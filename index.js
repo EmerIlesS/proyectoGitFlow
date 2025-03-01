5a1e87fe485fdf2ddd778ad86e45cff3ac76b18b
EmerIlesS <emersonilesitp@gmail.com>
2025-03-01 18:26:17 -0500

    agregar escript

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function login( username, password){
         return "invalid usermane or password"
     }
 }
+
+function login( username, password){
+    if (username==="admin" && password==="password123") {
+        return"Login successful!"
+        
+    }else{
+        return "invalid usermane or password"
+    }
+}
